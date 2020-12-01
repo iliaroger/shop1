90a6387a197e52aad6d3cabc1d6996358bbfa44b
Ilia Roger <ilia@ilias-air.speedport.ip>
2020-11-30 20:27:38 -0800

    init files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Hello world!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;

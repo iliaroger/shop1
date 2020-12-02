6e911f2624d09109ad212d219908ae281d76cbaf
Ilia Roger <ilia@ilias-air.speedport.ip>
2020-12-02 14:31:35 -0800

    minor changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import './App.css';
+import './styles/app.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <div>
         <h1>Hello world!</h1>
       </div>
---
+++ frontend/src/views/DetailedProductView.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function DetailedProductView() {
+  return <div></div>;
+}
